48be7e29a533dda98c28ca42768fa3e2aab610a9
t29mato <tomoya.matou@gmail.com>
2021-05-27 17:02:43 +0900

    add: database list screen empty file

---
+++ pages/databases/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    this is databases list screen
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => {
+    return {
+      json: ''
+    }
+  },
+  mounted() {
+    console.log("It's mounted.")
+    if (localStorage.json) {
+      this.json = localStorage.json;
+    }
+  },
+  watch: {
+    json(newJson) {
+      console.log("It's called json")
+      localStorage.json = newJson;
+    }
+  }
+})
+</script>
