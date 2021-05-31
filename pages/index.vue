7e6248af5fd990c4c47d3658f11d0e4e552042df
t29mato <tomoya.matou@gmail.com>
2021-05-31 06:36:35 +0000

    lint

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <a href="/wpd/" target="_blank">WebPlotDigitizerを開く</a><br>
+    <a href="/wpd/" target="_blank">WebPlotDigitizerを開く</a><br />
     {{ json }}
     <!-- <button v-on:click="hoge()">hoge</button> -->
   </div>
@@ -12,20 +12,20 @@ import Vue from 'vue'
 export default Vue.extend({
   data: () => {
     return {
-      json: ''
-    }
-  },
-  mounted() {
-    console.log("It's mounted.")
-    if (localStorage.json) {
-      this.json = localStorage.json;
+      json: '',
     }
   },
   watch: {
     json(newJson) {
-      console.log("It's called json")
-      localStorage.json = newJson;
+      console.error("It's called json")
+      localStorage.json = newJson
+    },
+  },
+  mounted() {
+    console.error("It's mounted.")
+    if (localStorage.json) {
+      this.json = localStorage.json
     }
-  }
+  },
 })
 </script>
