c6d0e2d9ea475d4cd2e4445ace182360b9b33373
t29mato <tomoya.matou@gmail.com>
2021-06-10 08:54:12 +0000

    refactor: connecting API-mock using API-client generated by OpenAPI Generator

--- pages/databases/index.vue
+++ pages/databases/index.vue
@@ -15,6 +15,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { DefaultApiFactory, Project } from '@/api/out'
 
 @Component
 export default class DatabasesScreen extends Vue {
@@ -22,96 +23,25 @@ export default class DatabasesScreen extends Vue {
   sortDesc = false
   get headers() {
     return [
-      { text: 'ID', value: 'ID' },
-      { text: 'Database', value: 'database' },
-      { text: 'Papers', value: 'papers' },
-      { text: 'Figures', value: 'Figures' },
-      { text: 'Samples', value: 'Samples' },
-      { text: 'My lists', value: 'My lists' },
+      { text: 'ID', value: 'id' },
+      { text: 'Database', value: 'attributes.name' },
+      { text: 'Papers', value: 'relationships.papers.meta.totalPapers' },
+      { text: 'Figures', value: 'relationships.figures.meta.totalFigures' },
+      { text: 'Samples', value: 'relationships.samples.meta.totalSamples' },
     ]
   }
 
-  items: {
-    ID: number
-    database: string
-    papers: number
-    papersWithData: number
-    Figures: number
-    Samples: number
-    'My lists': number
-  }[] = [
-    {
-      database: 'General DB',
-      ID: 1,
-      papers: 30449,
-      papersWithData: 7333,
-      Figures: 31480,
-      Samples: 40458,
-      'My lists': 80,
-    },
-    {
-      database: 'Thermoelectric Materials',
-      ID: 2,
-      papers: 23870,
-      papersWithData: 6319,
-      Figures: 29260,
-      Samples: 34513,
-      'My lists': 1,
-    },
-    {
-      database: 'Magnetic Materials',
-      ID: 3,
-      papers: 4103,
-      papersWithData: 714,
-      Figures: 1494,
-      Samples: 3849,
-      'My lists': 1,
-    },
-    {
-      database: 'Condensed Matter',
-      ID: 4,
-      papers: 394,
-      papersWithData: 132,
-      Figures: 509,
-      Samples: 1374,
-      'My lists': 1,
-    },
-    {
-      database: 'High Thermal Conductivity Materials',
-      ID: 5,
-      papers: 179,
-      papersWithData: 124,
-      Figures: 250,
-      Samples: 519,
-      'My lists': 0,
-    },
-    {
-      database: 'Piezoelectric Materials',
-      ID: 6,
-      papers: 134,
-      papersWithData: 0,
-      Figures: 0,
-      Samples: 0,
-      'My lists': 0,
-    },
-    {
-      database: 'Hyper Material',
-      ID: 7,
-      papers: 76,
-      papersWithData: 55,
-      Figures: 146,
-      Samples: 262,
-      'My lists': 0,
-    },
-    {
-      database: 'Project TtoH',
-      ID: 8,
-      papers: 0,
-      papersWithData: 0,
-      Figures: 0,
-      Samples: 0,
-      'My lists': 0,
-    },
-  ]
+  created() {
+    const api = DefaultApiFactory(undefined, 'http://localhost:4010')
+    const projects = api.getProjects()
+    projects.then((items) => {
+      if (items.data.data) {
+        const projects: Project[] = items.data.data
+        this.items = projects
+      }
+    })
+  }
+
+  items: Project[] = []
 }
 </script>
