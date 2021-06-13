2c18391de382a0750a7ed3816d9de954268efd23
t29mato <tomoya.matou@gmail.com>
2021-06-13 13:32:24 +0000

    refactor: use object component instaed of class component

--- pages/databases/index.vue
+++ pages/databases/index.vue
@@ -14,34 +14,37 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import Vue from 'vue'
 import { DefaultApiFactory, Database } from '@/api/out'
 
-@Component
-export default class DatabasesScreen extends Vue {
-  sortBy = 'age'
-  sortDesc = false
-  get headers() {
-    return [
-      { text: 'ID', value: 'id' },
-      { text: 'Database', value: 'attributes.name' },
-      { text: 'Papers', value: 'relationships.papers.meta.totalPapers' },
-      { text: 'Figures', value: 'relationships.figures.meta.totalFigures' },
-      { text: 'Samples', value: 'relationships.samples.meta.totalSamples' },
-    ]
-  }
-
+export default Vue.extend({
+  data() {
+    return {
+      sortBy: 'age',
+      sortDesc: false,
+      items: [] as Database[],
+    }
+  },
+  computed: {
+    headers() {
+      return [
+        { text: 'ID', value: 'id' },
+        { text: 'Database', value: 'attributes.name' },
+        { text: 'Papers', value: 'relationships.papers.meta.totalPapers' },
+        { text: 'Figures', value: 'relationships.figures.meta.totalFigures' },
+        { text: 'Samples', value: 'relationships.samples.meta.totalSamples' },
+      ]
+    },
+  },
   created() {
     const api = DefaultApiFactory(undefined, process.env.STARRYDATA_API_URL)
     const projects = api.getDatabases()
     projects.then((items) => {
       if (items.data.data) {
-        const projects: Database[] = items.data.data
+        const projects = items.data.data
         this.items = projects
       }
     })
-  }
-
-  items: Database[] = []
-}
+  },
+})
 </script>
