b623137d0d671938e2b36876595efa5c040f1ad5
t29mato <tomoya.matou@gmail.com>
2021-06-14 06:49:16 +0000

    feat: add paper list page

--- pages/papers/index.vue
+++ pages/papers/index.vue
@@ -1,112 +1,75 @@
 <template>
-  <div class="flex items-start justify-between">
-    <Sidebar></Sidebar>
-    <div class="flex flex-col w-full md:space-y-4">
-      <Header></Header>
-      <div class="overflow-auto h-screen pb-24 px-4 md:px-6">
-        <div class="overflow-auto">
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="rows()"
-            :per-page="perPage"
-            aria-controls="my-table"
-          ></b-pagination>
-
-          <p class="mt-3">Current Page: {{ currentPage }}</p>
-
-          <b-table
-            id="my-table"
-            :items="items"
-            :per-page="perPage"
-            :current-page="currentPage"
-            small
-          ></b-table>
-        </div>
-      </div>
-    </div>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      item-key="SID"
+      class="elevation-1"
+    >
+      <template #[`item.attributes.authors`]="{ item }">
+        {{ makeAuthorsOneLine(item.attributes.authors) }}
+      </template>
+      <template #[`item.attributes.title`]="{ item }">
+        {{ item.attributes.title | truncate(120) }}
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import Vue from 'vue'
+import { Contributor, DefaultApiFactory, Paper } from '@/api/out'
 
-@Component
-export default class PaperList extends Vue {
-  perPage = 3
-  currentPage = 1
-  items: {
-    SID: number
-    title: string
-    authors: string
-    identifier: string
-    DOI: string
-    figures: number
-    samples: number
-  }[] = [
-    {
-      SID: 1,
-      title:
-        'Decoupling Interrelated Parameters for Designing High Performance Thermoelectric Materials',
-      authors: 'C. Xiao, Z. Li, K. Li, P. Huang, Y. Xie,',
-      identifier: '00000001_Xiao_2014',
-      DOI: '10.1021/ar400290f',
-      figures: 1,
-      samples: 1,
+export default Vue.extend({
+  filters: {
+    truncate(text: string, end: number): string {
+      if (text.length > end) {
+        return text.slice(0, end) + '...'
+      }
+      return text
     },
-    {
-      SID: 2,
-      title:
-        'Decoupling Interrelated Parameters for Designing High Performance Thermoelectric Materials',
-      authors: 'C. Xiao, Z. Li, K. Li, P. Huang, Y. Xie,',
-      identifier: '00000001_Xiao_2014',
-      DOI: '10.1021/ar400290f',
-      figures: 1,
-      samples: 1,
+  },
+  data() {
+    return {
+      sortBy: 'SID',
+      sortDesc: false,
+      items: [] as Paper[],
+    }
+  },
+  computed: {
+    headers() {
+      return [
+        { text: 'SID', value: 'id' },
+        { text: 'DOI', value: 'attributes.DOI' },
+        { text: 'title', value: 'attributes.title' },
+        { text: 'author', value: 'attributes.authors' },
+        { text: 'Journal', value: 'attributes.container_title' },
+        { text: 'Publisher', value: 'attributes.publisher' },
+        { text: 'Figures', value: 'relationships.figures.meta.total' },
+        { text: 'Samples', value: 'relationships.samples.meta.total' },
+      ]
     },
-    {
-      SID: 3,
-      title:
-        'Decoupling Interrelated Parameters for Designing High Performance Thermoelectric Materials',
-      authors: 'C. Xiao, Z. Li, K. Li, P. Huang, Y. Xie,',
-      identifier: '00000001_Xiao_2014',
-      DOI: '10.1021/ar400290f',
-      figures: 1,
-      samples: 1,
+  },
+  created() {
+    const api = DefaultApiFactory(undefined, process.env.STARRYDATA_API_URL)
+    const papers = api.getPapers()
+    papers.then((items) => {
+      if (items.data.data) {
+        const papers = items.data.data
+        this.items = papers
+      }
+    })
+  },
+  methods: {
+    makeAuthorsOneLine(authors: Contributor[]): string {
+      return authors.reduce((prev, cur, index, array) => {
+        const result = prev + cur.family + ' ' + cur.given
+        if (index === array.length - 1) {
+          return result
+        }
+        return result + ', '
+      }, '')
     },
-    {
-      SID: 4,
-      title:
-        'Decoupling Interrelated Parameters for Designing High Performance Thermoelectric Materials',
-      authors: 'C. Xiao, Z. Li, K. Li, P. Huang, Y. Xie,',
-      identifier: '00000001_Xiao_2014',
-      DOI: '10.1021/ar400290f',
-      figures: 1,
-      samples: 1,
-    },
-    {
-      SID: 5,
-      title:
-        'Decoupling Interrelated Parameters for Designing High Performance Thermoelectric Materials',
-      authors: 'C. Xiao, Z. Li, K. Li, P. Huang, Y. Xie,',
-      identifier: '00000001_Xiao_2014',
-      DOI: '10.1021/ar400290f',
-      figures: 1,
-      samples: 1,
-    },
-    {
-      SID: 6,
-      title:
-        'Decoupling Interrelated Parameters for Designing High Performance Thermoelectric Materials',
-      authors: 'C. Xiao, Z. Li, K. Li, P. Huang, Y. Xie,',
-      identifier: '00000001_Xiao_2014',
-      DOI: '10.1021/ar400290f',
-      figures: 1,
-      samples: 1,
-    },
-  ]
-
-  public rows() {
-    return this.items.length
-  }
-}
+  },
+})
 </script>
