5935f9d96b23d15bf9c545d2b4b31ca38f7f451d
t29mato <tomoya.matou@gmail.com>
2021-06-18 08:42:13 +0000

    feat(Data): add template page

---
+++ pages/papers/data/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container> </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  computed: {},
+  mounted() {},
+  created() {},
+  methods: {},
+})
+</script>
