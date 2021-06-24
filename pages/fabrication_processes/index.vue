a0ccc28f709345a3f67027d9872e27ef4fbd38b5
t29mato <tomoya.matou@gmail.com>
2021-06-24 10:08:35 +0000

    feat(FabricationProcesses): add delete and create features template

--- pages/fabrication_processes/index.vue
+++ pages/fabrication_processes/index.vue
@@ -1,16 +1,47 @@
 <template>
   <v-container>
-    <v-treeview :items="items"></v-treeview>
+    <v-dialog v-model="dialog">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          {{ selectedItem.id + ': ' + selectedItem.name }}
+        </v-card-title>
+
+        <v-card-text> text text text text text text text text </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-input v-model="parentId"></v-input>
+          <v-btn color="error" text @click="createItem()"> Create </v-btn>
+
+          <v-btn color="error" text @click="deleteItem(selectedItem.id)">
+            Delete
+          </v-btn>
+          <v-btn color="primary" text @click="dialog = false"> Close </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-treeview :items="items">
+      <template slot="label" slot-scope="{ item }">
+        <a @click="openDialog(item)">{{ item.name }}</a>
+      </template>
+    </v-treeview>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { DefaultApiFactory, FabricationProcess } from '@/api/out'
+import {
+  DefaultApiFactory,
+  FabricationProcess,
+  FabricationProcessesData,
+} from '@/api/out'
 
+// FIX: nameは必須のはず
 type TreeViewItem = {
-  id?: string
-  name?: string
+  id: string
+  name: string
   children?: TreeViewItem[]
 }
 
@@ -26,6 +57,9 @@ export default Vue.extend({
   data() {
     return {
       items: [] as TreeViewItem[],
+      selectedItem: {} as TreeViewItem,
+      dialog: false,
+      parentId: '',
     }
   },
   computed: {},
@@ -33,6 +67,45 @@ export default Vue.extend({
     this.loadFabricationProcesses()
   },
   methods: {
+    openDialog(e: TreeViewItem) {
+      this.dialog = true
+      this.selectedItem = e
+    },
+    async deleteItem(id: string) {
+      const api = DefaultApiFactory(undefined, process.env.STARRYDATA_API_URL)
+      try {
+        await api.deleteFabricationProcessesFabricationProcessId(id)
+        this.dialog = false
+        this.loadFabricationProcesses()
+      } catch {
+        //
+      } finally {
+        //
+      }
+    },
+    async createItem() {
+      const item: FabricationProcessesData = {
+        type: 'FabricationProcess',
+        id: '', // FIXME: nullableのはず
+        attributes: {
+          name_ja: this.selectedItem.name,
+          parent_id: {
+            type: 'FabricationProcess',
+            id: this.selectedItem.id,
+          },
+        },
+      }
+      const api = DefaultApiFactory(undefined, process.env.STARRYDATA_API_URL)
+      try {
+        await api.postFabricationProcesses({ data: item })
+        this.dialog = false
+        this.loadFabricationProcesses()
+      } catch {
+        //
+      } finally {
+        //
+      }
+    },
     generateTree(
       treeViewItem: TreeViewItem,
       fabricationProcesses: FabricationProcess[]
